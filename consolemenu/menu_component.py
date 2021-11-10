e6f51e536c1835ed9b8afc3d56ba706d1dbb8d5c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-11-10 14:05:28 -0500

    textwrapping is now handled within the row function

--- consolemenu/menu_component.py
+++ consolemenu/menu_component.py
@@ -269,8 +269,7 @@ class MenuTextSection(MenuComponent):
         for x in range(0, self.padding.top):
             yield self.row()
         if self.text is not None and self.text != '':
-            for line in textwrap.wrap(self.text, width=self.calculate_content_width()):
-                yield self.row(content=line, align=self.text_align)
+            yield self.row(content=self.text, align=self.text_align)
         for x in range(0, self.padding.bottom):
             yield self.row()
         if self.show_bottom_border:
